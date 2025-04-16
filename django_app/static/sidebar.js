38ba63b8f06eaef8e12b5c0120e9a783d3934ec2
Samir Malik <samirmalik@Samirs-MacBook-Air.local>
2025-04-16 21:24:11 +0400

    boostrap side bar

---
+++ django_app/static/sidebar.js
@@ -0,0 +1,7 @@
+document.querySelector("#sidebar").classList.toggle("expand");
+
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});
