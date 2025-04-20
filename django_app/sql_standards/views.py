01db783b06640f25194321651f5edcf72a4e376f
Samir Malik <samirmalik@MacBookAir-2.broadband>
2025-04-20 11:15:31 +0100

    send editor text via htmx

--- django_app/sql_standards/views.py
+++ django_app/sql_standards/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import TemplateView
-from django.views.generic import View
-from django.http import JsonResponse
 
 class SqlStandardsView(TemplateView):
     template_name = "sql_standards.html"
 
 class SqlStandardsResultView(TemplateView):
     template_name = "sql_standards_results.html"
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
